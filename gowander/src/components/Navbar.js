07c2aa6d6867f685f08a7241c25fbb25811318c4
Eden Smith <edenwint@gmail.com>
2020-12-15 20:50:55 +0000

    nav bar icons/ names added- working on button styling

--- gowander/src/components/Navbar.js
+++ gowander/src/components/Navbar.js
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+
+
+
+
     return (
         <>
             <nav className="navbar">
@@ -11,6 +20,33 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         GoWander <i className="fab fa-typo3"></i>
                     </Link>
+                    {/* toggle between bars navbar */}
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times': 'fas fa-bars'} />
+                    </div>
+                    {/* when on mobile navbar disappear */}
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                HOME
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                ABOUT
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/how-it-works' className='nav-links' onClick={closeMobileMenu}>
+                                HOW IT WORKS
+                            </Link>
+                        </li>
+                            <li className='nav-item'>
+                            <Link to='/get-started' className='nav-links' onClick={closeMobileMenu}>
+                                Get Started
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
             
