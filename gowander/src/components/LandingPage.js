909812aecbe201f191565d962233a0452cfdb946
Eden Smith <edenwint@gmail.com>
2020-12-20 00:31:41 +0000

    ahve image now showing on landing / home page

---
+++ gowander/src/components/LandingPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './LandingPage.css';
+
+function LandingPage() {
+    return (
+        <div className="landing-container">
+            <h1>Let's Go</h1>
+            <p>What are you waiting for?</p>
+            <div className="lanidng-btns">
+                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
+                    GET STARTED
+                </Button>
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
+                    GET STARTED
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ gowander/src/components/Pages/Home.js
@@ -0,0 +1,18 @@
+import '../../App.css';
+import React from 'react'
+import '../LandingPage';
+import LandingPage from '../LandingPage';
+
+
+
+
+function Home() {
+    return (
+        <>
+            <LandingPage />
+            
+        </>
+    )
+}
+
+export default Home;
