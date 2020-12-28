a860fff4afb59b0d0bdfd69ca29a2d416f17795d
Eden Smith <edenwint@gmail.com>
2020-12-28 04:49:17 +0000

    about page info added. css updtaed for about/ how it works page

--- gowander/src/components/Pages/About.js
+++ gowander/src/components/Pages/About.js
@@ -8,8 +8,10 @@ function About() {
         <div className="about-container">
             <h1>What is GoWander? </h1>
             <p>
-                The idea of GoWonder, is a fun way to explore a city. Simply type in your location, add a filter here and there, press the button, and it will generate an activity for you to do based on your filters and preferences.
-                For now the app will focus on Birmingham. 
+            Have you ever visited a new city and had nothing to do? Are you bored and looking for new and different to do in your current home city? Then GoWander is the answer to all your problems. Whether you are after something do with the family, in nature or just something completely random then look further. 
+            Simply pick a filter and GoWander will generate a variety of activities for you to do based on your preferences.
+            For now, we are all operating within Birmingham UK, but so are looking to expand further across the UK.  
+
             </p>
             <div className="about-btns">
                 <NewButton className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
