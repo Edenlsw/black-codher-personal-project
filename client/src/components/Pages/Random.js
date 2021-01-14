01d23f6c2b441f7e23660c491ce16e4be478e3bc
Eden Smith <edenwint@gmail.com>
2021-01-14 22:28:09 +0000

    random page now filtered correctly

--- client/src/components/Pages/Random.js
+++ client/src/components/Pages/Random.js
@@ -7,65 +7,69 @@ import PropTypes from 'prop-types';
 
 
 function Random() {
+    
   const [filters, setfilters] = useState(null);
-  const [activityFilters, setActivityFilters] = useState(null);
-    
-     
-    
-      
-  const getfilters = async () => {
-    let res = await filterService.getAll();
-    setfilters(res);
-    setActivityFilters(res.filter(e => e.type === "random"));
+  // const [filteredActivity, setFilteredActivity] = useState(null)
+  
 
+
+  useEffect(() => {
+          if (!filters) {
+            getfilters();
+          }
+        });
+      
+        const getfilters = async () => {
+          let res = await filterService.getAll();
+          // setfilters(res);
+          console.log(res)
+          const filtered = res.filter(e => (e.filterCategory.includes("random")));
+          console.log(filtered)
+          setfilters(filtered);
   };
   
-  // fetching data from database
-  useEffect(() => {
-    if (!filters) {
-      getfilters();
-    }
-  });
-    
-  // var random = filters.filter(e => e.type === "random")
-  
-  function showRandom() {
-    setActivityFilters()
-  }
+
   
   
-  // const renderFilter = (filter) 
+  
+   
+
+
+    // function showFamily() {
+    //   setFilteredActivity(Family)
+    // }
+
     
-        
-      
-    
+    // const [filteredActivity, setFilteredActivity] = useState(null)
+  
+  
     return (
-      <div className="random">
-        <button onClick={() => showRandom()}>Random</button>
-          
-        {
-          activityFilters && activityFilters.map((filter) => (
-            <div key={filter._id}>
-               <h3>
+        <div className="filters">
+            {/* <button onClick={() => showFamily()}>Family</button> */}
+            
+
+            {
+                filters && filters.map((filter) => (
+                    <div key={filter._id}>
+                    <h3>
                       {filter.activityName}
                     </h3>
                     <p>{filter.description}</p>
                     
-              <filterService filter={filter} />
-              <hr style={{ opacity: '.1' }} />
-            </div>
-          ))
-        }
-      </div>
+                    </div>
+
+                ))
+            }
+        </div>
+
     );
-          
-  }
+}
 
 
   
   
   
-    export default Random;  
+    export default Random;
 
 
 
@@ -77,63 +81,3 @@ function Random() {
 
 
 
-
-
-
-
-
-// function Random() {
-//   const [filters, setfilters] = useState(null);
-//   const [activityFilters, setActivityFilters] = useState(null);
-    
-     
-    
-      
-//   const getfilters = async () => {
-//     let res = await filterService.getAll();
-//     setfilters(res);
-//     setActivityFilters(res.filter(e => e.type === "active"));
-
-//   };
-  
-//   // fetching data from database
-//   useEffect(() => {
-//     if (!filters) {
-//       getfilters();
-//     }
-//   });
-    
-//   var active = filters.filter(e => e.type === "active")
-  
-//   function showActive() {
-//     setActivityFilters(active)
-//   }
-  
-  
-//   // const renderFilter = (filter) 
-    
-        
-      
-    
-//     return (
-//       <div className="active">
-//         <button onClick={() => showActive()}>active</button>
-          
-//         {
-//           activityFilters && activityFilters.map((filter) => (
-//             <div key={filter._id}>
-//               <filterService filter={filter} />
-//               <hr style={{ opacity: '.1' }} />
-//             </div>
-//           ))
-//         }
-//       </div>
-//     );
-          
-//   }
-
-
-  
-  
-  
-//     export default Random;  
